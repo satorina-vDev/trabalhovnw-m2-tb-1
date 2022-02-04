ce2ee5582b036285bb0fea020978b6c0b2d96d80
satorina-vDev <nonato.miau@hotmail.com>
2022-02-04 02:34:34 -0300

    trabalho1

---
+++ script.js
@@ -0,0 +1,9 @@
+let nome = "tchamp"
+let numero = 7
+let ano = 2022 - numero
+let resultado = numero + ano
+console.log(resultado)
+console.log("Estou mexendo no console.log")
+let quartaFeira = true 
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
